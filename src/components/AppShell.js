1fd02c4088cdc524626d63cd6d90dcb12728c71b
Josh Bradley <joshuabradley012@gmail.com>
2020-08-13 21:04:48 -0700

    Added nav

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,12 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useState
+} from 'react';
+import { generatePath } from 'react-router';
 import {
+  BrowserRouter as Router,
   Link,
-  Router,
+  NavLink as ActiveLink,
+  Redirect,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch,
 } from 'react-router-dom';
 import classNames from 'classnames';
 import Logo from '../assets/remodeler.svg';
 import Bell from 'bootstrap-icons/icons/bell-fill.svg' ;
+import Cart from 'bootstrap-icons/icons/cart-fill.svg' ;
 import Person from 'bootstrap-icons/icons/person-fill.svg' ;
+import Toggles from 'bootstrap-icons/icons/toggles.svg' ;
 
 const projects = [
   {
@@ -20,7 +33,7 @@ const projects = [
   {
     id: 'kitchen',
     name: 'Kitchen',
-    image: 'https://i.pinimg.com/474x/80/44/4d/80444dc2cdd4468e96f071e700e4e409.jpg',
+    image: 'https://i.pinimg.com/236x/80/44/4d/80444dc2cdd4468e96f071e700e4e409.jpg',
     status: 'success',
     budgetSpent: 600,
     budgetTotal: 1400,
@@ -83,6 +96,72 @@ const projects = [
   },
 ];
 
+const products = [
+  {
+    id: 'bathroom-sink',
+    name: 'Bathroom Sink',
+    image: 'https://i.pinimg.com/236x/95/6c/cb/956ccb1ad964ea354f21f7897d844366.jpg',
+    price: 400,
+    supplier: 'Home depot',
+  },
+  {
+    id: 'kitchen-counter',
+    name: 'Kitchen Counter',
+    image: 'https://i.pinimg.com/236x/80/44/4d/80444dc2cdd4468e96f071e700e4e409.jpg',
+    price: 1400,
+    supplier: 'Amazon',
+  },
+  {
+    id: 'coffee-table',
+    name: 'Coffee table',
+    image: 'https://i.pinimg.com/236x/bd/73/b6/bd73b6c23df1721f47228746a0b2689f.jpg',
+    price: 300,
+    supplier: 'Wayfair',
+  },
+  {
+    id: 'rug',
+    name: 'Rug',
+    image: 'https://i.pinimg.com/236x/83/02/d5/8302d507ff58af5e1593127c8f390106.jpg',
+    price: 200,
+    supplier: 'Pier 21',
+  },
+  {
+    id: 'washer',
+    name: 'Washer',
+    image: 'https://i.pinimg.com/236x/d8/4d/51/d84d5185369604c2d0f368651dacb0b7.jpg',
+    price: 900,
+    supplier: 'Lowes',
+  },
+  {
+    id: 'desk',
+    name: 'Desk',
+    image: 'https://i.pinimg.com/236x/9b/b1/85/9bb1852452d5a1e11098ebf3a3997465.jpg',
+    price: 650,
+    supplier: 'Ikea',
+  },
+  {
+    id: 'patio-furniture-set',
+    name: 'Patio furniture set',
+    image: 'https://i.pinimg.com/236x/45/af/f5/45aff59cfd585a364febfc690c0fa310.jpg',
+    price: 1400,
+    supplier: 'ULINE',
+  },
+  {
+    id: 'pavers',
+    name: 'Pavers',
+    image: 'https://i.pinimg.com/236x/44/ed/ed/44ededa34faf0f4e639c18fcb221aaa7.jpg',
+    price: 400,
+    supplier: 'Home depot',
+  },
+  {
+    id: 'gravel',
+    name: 'Gravel',
+    image: 'https://i.pinimg.com/236x/88/a0/23/88a023e994f3c319ab2e0233a02dca9e.jpg',
+    price: 100,
+    supplier: 'Mission ready mix',
+  },
+];
+
 const useResize = ref => {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
@@ -118,23 +197,39 @@ const useResize = ref => {
   return { width, height, innerWidth, innerHeight };
 };
 
-const Project = ({ item }) => {
+const ProjectItem = ({ item }) => {
   const { id, name, image, status, budgetSpent, budgetTotal } = item;
   return (
-    <div className="project">
-      <a className="project-link" href={`/project/${id}`}>
-        <img className="project-image" src={image} />
-        <div className="project-text">
-          <h3 className="project-name">{name}</h3>
-          <span title="Project status" className={`project-status bg-${status}`} />
-          <p className="project-budget">{`$${budgetSpent} / $${budgetTotal}`}</p>
-        </div>
-      </a>
-    </div>
+    <Link className="project-item" to={`/project/${id}`}>
+      <div className="project-item-image-wrapper">
+        <img className="project-item-image" src={image} />
+      </div>
+      <div className="project-item-text">
+        <h3 className="project-item-name">{name}</h3>
+        <span title="Project status" className={`project-item-status bg-${status}`} />
+        <p className="project-item-budget">{`$${budgetSpent} / $${budgetTotal}`}</p>
+      </div>
+    </Link>
   )
 };
 
-const MasonryGrid = ({ children = [], minColumns = 2, itemWidth = 240, gridGap = 16 }) => {
+const ShopItem = ({ item }) => {
+  const { id, name, image, price, supplier } = item;
+  return (
+    <Link className="shop-item" to={`/shop/${id}`}>
+      <div className="shop-item-image-wrapper">
+        <img className="shop-item-image" src={image} />
+      </div>
+      <div className="shop-item-text">
+        <span className="shop-item-price">{`$${price}`}</span>
+        <h3 className="shop-item-name">{name}</h3>
+        <p className="shop-item-supplier">{supplier}</p>
+      </div>
+    </Link>
+  )
+};
+
+const MasonryGrid = ({ children = [], className, minColumns = 2, itemWidth = 240, gridGap = 16 }) => {
   const [columnCount, setColumnCount] = useState(0);
   const ref = useRef(null);
   const { innerWidth } = useResize(ref);
@@ -151,23 +246,23 @@ const MasonryGrid = ({ children = [], minColumns = 2, itemWidth = 240, gridGap =
   };
 
   const itemsInColumns = () => {
-    const childrenInColumns = [];
+    const items = [];
     for (let i = 0; i < children.length; i++) {
       const columnIndex = i % columnCount;
 
-      if (!childrenInColumns[columnIndex]) {
-        childrenInColumns[columnIndex] = [];
+      if (!items[columnIndex]) {
+        items[columnIndex] = [];
       }
 
-      childrenInColumns[columnIndex].push(children[i]);
+      items[columnIndex].push(children[i]);
     }
-    return childrenInColumns;
+    return items;
   };
 
   const renderColumns = () => {
-    const childrenInColumns = itemsInColumns();
+    const columns = itemsInColumns();
 
-    return childrenInColumns.map((items, i) => {
+    return columns.map((items, i) => {
       return (
         <div className="masonry-column" key={i}>
           {items}
@@ -186,28 +281,110 @@ const MasonryGrid = ({ children = [], minColumns = 2, itemWidth = 240, gridGap =
   };
 
   return (
-    <div ref={ref} style={gridStyle} className="masonry-grid">
+    <div ref={ref} style={gridStyle} className={classNames(className, 'masonry-grid')}>
       {renderColumns()}
     </div>
   );
 };
 
-const NavButton = ({ className, ...props }) => (
-  <button className={classNames(className, 'nav-button')} {...props} />
+const Home = () => (
+  <MasonryGrid className="home">
+    {projects.map((project, i) => <ProjectItem item={project} key={i} />)}
+  </MasonryGrid>
 );
+
+const Projects = () => (
+  <div className="project-dashboard all">
+    <h1 className="project-title">Projects</h1>
+    <div className="project-actions">
+      <NavLink exact to="/account">Active</NavLink>
+      <NavLink to="/account/finished">Finished</NavLink>
+    </div>
+    <MasonryGrid className="projects">
+      {projects.map((project, i) => <ProjectItem item={project} key={i} />)}
+    </MasonryGrid>
+  </div>
+);
+
+const Shop = () => (
+  <div className="shop-dashboard all">
+    <h1 className="shop-title">Shop</h1>
+    <div className="shop-actions">
+      <NavLink exact to="/shop">All</NavLink>
+      <NavLink to="/shop/popular">Popular</NavLink>
+      <NavLink to="/shop/deals">Deals</NavLink>
+      <NavButton className="toggle">
+        <Toggles className="button-icon black" />
+      </NavButton>
+    </div>
+    <MasonryGrid className="shop">
+      {products.map((product, i) => <ShopItem item={product} key={i} />)}
+    </MasonryGrid>
+  </div>
+);
+
+const NavButton = ({ to, className, ...props }) => (
+  <Link to={to} className={classNames(className, 'nav-button')} {...props} />
+);
+
+const NavLink = ({ to, className, ...props }) => (
+  <ActiveLink to={to} className={classNames(className, 'nav-link')} {...props} />
+);
+
+const Project = () => {
+  const match = useRouteMatch();
+  const { id } = useParams();
+
+  const project = projects.find(project => project.id === id);
+  const actions = [
+    {
+      id: 'ideas',
+      name: 'Ideas',
+    },
+    {
+      id: 'orders',
+      name: 'Orders',
+    },
+    {
+      id: 'status',
+      name: 'Status',
+    },
+  ];
+
+  return (
+    <div className="project-dashboard single">
+      <h1 className="project-title">{project.name}</h1>
+      <div className="project-actions">
+        {actions.map((action, i) => {
+          const actionLink = generatePath(match.path, { id: project.id, action: action.id });
+          return <NavLink to={actionLink} key={i}>{action.name}</NavLink>;
+        })}
+      </div>
+      <MasonryGrid>
+        {projects.map((project, i) => <ProjectItem item={project} key={i} />)}
+      </MasonryGrid>
+    </div>
+  );
+};
 
 const AppBar = () => (
   <div className="app-bar">
-    <NavButton className="logo">
+    <NavButton to="/" className="logo">
       <Logo className="button-icon" />
     </NavButton>
+    <NavLink exact to="/">Home</NavLink>
+    <NavLink to="/shop">Shop</NavLink>
+    <NavLink to="/hire">Hire</NavLink>
     <form id="search">
       <input placeholder="Search" />
     </form>
-    <NavButton>
+    <NavButton to="/notifications">
       <Bell className="button-icon" />
     </NavButton>
-    <NavButton>
+    <NavButton to="/cart">
+      <Cart className="button-icon" />
+    </NavButton>
+    <NavButton to="/account">
       <Person className="button-icon" />
     </NavButton>
   </div>
@@ -215,12 +392,18 @@ const AppBar = () => (
 
 const AppShell = () => {
   return (
-    <div className="app-shell">
-      <AppBar />
-      <MasonryGrid>
-        {projects.map((project, i) => <Project item={project} key={i} />)}
-      </MasonryGrid>
-    </div>
+    <Router>
+      <div className="app-shell">
+        <AppBar />
+        <Switch>
+          <Redirect exact from="/project/:id" to="/project/:id/ideas" />
+          <Route path="/project/:id/:action?" component={Project} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/account" component={Projects} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
